38b9be04f95d50a296a4f206549ceee860c7b73a
Randy Martinez <randym0624@gmail.com>
2022-07-03 21:37:15 -0400

    Cambié el nombre de las carpetas de la app de peliculas y la de la seccion de diseños, y tambien terminé de hacer los diseños

---
+++ 05- diseños/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:disenos/screens/basic_design.dart';
+import 'package:disenos/screens/scroll_design.dart';
+import 'package:disenos/screens/home_screen.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle.light );
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      theme: ThemeData.dark(),
+      initialRoute: 'home_screen',
+      routes: {
+        'basic_design' : ( _ ) => BasicDesignScreen(),
+        'scroll_screen': ( _ ) => ScrollScreen(), 
+        'home_screen'  : ( _ ) => HomeScreen(), 
+      },
+    );
+  }
+}
